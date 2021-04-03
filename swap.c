56f06d09ee8b216b6aae533778d95037ffde3cf8
Rishab-09 <80740536+Rishab-09@users.noreply.github.com>
2021-04-03 21:45:08 +0530

    Swapping Numbers
    This is a simple program to swap a two numbers without taking any third variable.

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+ int main()    
+{    
+int a=20, b=40;      
+printf("     Before Swap a=%d b=%d\n",a,b);      
+a=a+b;//a=60 (20+40)    
+b=a-b;//b=20 (60-40)    
+a=a-b;//a=40 (60-20)    
+printf("     After Swap a=%d b=%d",a,b);    
+return 0;  
+}   
+
+
+
+
+
+
+
+
+
+
