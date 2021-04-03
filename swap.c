8ae9bcd9381cd5f80a031907e5e876e416c8b37a
Rishab-09 <80740536+Rishab-09@users.noreply.github.com>
2021-04-03 21:39:51 +0530

    swap.c

--- swap.c
+++ swap.c
@@ -2,11 +2,11 @@
  int main()    
 {    
 int a=20, b=40;      
-printf("     Before Swap a=%d b=%d\n",a,b);      
+printf("Before Swap a=%d b=%d\n",a,b);      
 a=a+b;//a=60 (20+40)    
 b=a-b;//b=20 (60-40)    
 a=a-b;//a=40 (60-20)    
-printf("     After Swap a=%d b=%d",a,b);    
+printf("After Swap a=%d b=%d",a,b);    
 return 0;  
 }   
 
